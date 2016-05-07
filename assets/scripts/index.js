d0bcb07463a742ba8e1191f07e0375a9bb2044dd
Amit Kumar <amit@Amits-MacBook-Pro.local>
2016-05-07 16:29:06 -0700

    definition database

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,20 +1,86 @@
 $(function(){
 	// Keys (aka ids) are the headlines. 
 	var explainerDB = {
-		'test' : {
-			headline : 'test headline',
-			explainer : 'test explainer',
-			triggers : ['notified to appear']
+		'Notified to appear' : {
+			headline : '"Notified to appear"',
+			explainer : 'Be sure to appear in court on the date specified. To create an automatic reminder for yourself, <a href="#">click here</a>.',
+			triggers : ['notified to appear', 'notice to appear']
+		},
+
+		'Arrested' : {
+			headline : '"Arrested"',
+			explainer : 'The Police took you to jail. This means that police have arrested you.',
+			triggers : ['Arrested']
+		},
+
+		'Promise to appear' : {
+			headline : '"Promise to appear"',
+			explainer : 'You signed a paper that says he promises to return to court at a specific date and time.',
+			triggers : ['Promise to appear']
+		},
+
+		'Trial' : {
+			headline : '"Trial"',
+			explainer : 'You goes to court for his trial with a lawyer to explain his side of the story.',
+			triggers : ['Trial']
+		},
+
+		'Arraignment' : {
+			headline : '"Arraignment"',
+			explainer : 'First time you appears in court. The judge will tell you what he is accused of, what his rights are. If you can’t pay for a lawyer , the court will  give one.',
+			triggers : ['Arraignment']
+		},
+
+		'Offered a deal' : {
+			headline : '"Offered a deal"',
+			explainer : 'At the arraignment, you can reply to the judge after learning what he is accused of. He can do that by a so called plea bargain. It’s a deal offered by the judge to avoid a full trial. you can chose to say plea guilty not guilty or no contest.',
+			triggers : ['Offered a deal']
+		},
+
+		'Admit to be guilty' : {
+			headline : '"Admit to be guilty"',
+			explainer : 'In a plea bargain, you can admit that he did the crime he ‘s accused of, it is called plead guilty. The judge will then decide that you is guilty and convict him. In reality you may be innocent.',
+			triggers : ['Admit to be guilty']
+		},
+
+		'Defense attorney' : {
+			headline : '"Defense attorney"',
+			explainer : 'You need to a lawyer to help explain his side of the story to the court. If you doesn’t have enough $ , he can ask for a free attorney. This is a public defender.',
+			triggers : ['Defense attorney']
+		},
+
+		'Law enforcement Officer' : {
+			headline : 'Leo ( Law enforcement Officer)',
+			explainer : 'Law enforcement officers try to stop people from breaking the law. If a Leo thinks you has broken the law, the Leo might Stop you and ask him questions or even take you to jail.',
+			triggers : ['leo', 'Law enforcement Officer']
+		},
+
+		'Judge' : {
+			headline : '"Judge"',
+			explainer : 'The person who is in charge of a trial in court. The judge decides how the person is guilty of a crime should be punished.',
+			triggers : ['Judge']
 		}
+
+		// Press charges: When the prosecutor accuses you of a crime, it also files charges
+		// Probation: It is an alternative to prison time, you may receive probation instead. If you behave well, you could avoid prison
+		// Probation officer: The PO is supervising you he is sentenced to probation
+		// Parole: Parole is an early release from prison if on parole, you is supervised by a parole officer
+		// Parole officer:  The parole officer is supervising you when he is released
+		// Prosecutor:  Lawyer who works closely with law enforcement officers (Leo). If the prosecutor thinks you broke the law, the prosecutor will file papers with the court that explains what laws you might have been broken.
+		// Jury: A jury is a small group of people from the community that watch you’s trial and decide if you broke the law. If you doesn’t want a jury, you can ask to have the judge to decide if you broke the law
+		// Sentence: The trial the judge or jury decides that you broke the law, they will tell if you broke the law= guilty. The judge will then have to decide how you is to be punished.
+		// Judge: 
+		// Bail: The money that you puts as a promise to return to a court for the trial
+
 	};
 
-	var triggerToRebuttalMap = {};
+	var triggerToExplainerMap = {};
 	_.each(explainerDB, function(explainer, explainerKey){
 		_.each(explainer.triggers, function(trigger){
-			triggerToRebuttalMap[trigger] = explainerKey;
+			triggerToExplainerMap[trigger] = explainerKey;
 		});
 	});
-	console.log('triggerToRebuttalMap', triggerToRebuttalMap);
+	console.log('triggerToExplainerMap', triggerToExplainerMap);
 
 
 
@@ -101,8 +167,7 @@ $(function(){
 						console.log('POST /scan-photo response', data);
 						
 						var text = '';
-						var carrier = 'Verizon';
-
+						
 						if (data.length && data[0].textAnnotations && data[0].textAnnotations.length){
 							text = data[0].textAnnotations[0].description;
 						}
@@ -110,14 +175,13 @@ $(function(){
 						$('#ocr-text').text(text);
 						$('.text-scan-results').text(text);
 
-						// appendClaimMatches(text, carrier);
+						appendClaimMatches(text);
 
 						$('.page').removeClass('active');
 						$('.page-results').addClass('active');
 					},
 					error : function error(err){
 						console.log('POST /scan-photo response err', err);
-						appendClaimMatches('', 'T-Mobile');
 						$('.page').removeClass('active');
 						$('.page-results').addClass('active');
 					}
@@ -132,38 +196,48 @@ $(function(){
 	}
 
 
-	function appendClaimMatches(text, carrier){
+	function appendClaimMatches(text){
 		var $list = $('.result-list');
 	
-		// text = "An invitation\nworth $650.\nWe'll pay for\nwhat's standing\nin your way.\nGet up to $650 when\nyou Switch your number,\ntrade in your phone and\nbuy a new one.\nLimited Time Offer $650 -$300 trade-in VZW gift card up to $350\nVisa prepaid card for switching fees. New 4G smartphone device\npayment activation and port-in from AT&T, TMobile, Sprint or U.S.\nCellular required. Accout must remain active for at least 45 days.\nTrade-in must be in good working condition\nVerizon\nOffer valid for activations in select Verizon wireless retail or authorized a\nlocations in Chicago, Rockford\", and Northwest Indiana Only. Limited Time 0ffer. Reimburesement for early termination fees or device\ninstallment buyoutcosts associated with switch will be paid with a Visa pre-paid card. Cards will be mailed via US Postal Service within 8 weeks after receipt of claim. Cards willbe mailed to the billing address on file\nwith Verizon Wireless. Cards will be issued in US dollars only. Cards are valid through the 6-month expiration date shown on the card. Cards are issued by Citibank not applicable pursuant to a license from Visa USA Inc\nand managed by Citi Prepaid Services. Cards will not have cash access and\ncan be used everywhere Visa debit cards are accepted. Maximum of 10 portin a\nvations/valid submissions peraccount. Subject to VZW\nagmts, Calling Plan and\ncredit approval. Offers and coverage, varying by swc, not available everywhere. See vzw.com While supplies last.\n";
 		var lowercasedStr = text.toLowerCase().replace(/(\n)+/g, ' ');
 
 		console.log('appendClaimMatches, matching on string ', lowercasedStr)
 		
 		
 		// get all claim keys, see if they match text in the result
-		var triggers = Object.keys(triggerToRebuttalMap);
+		var triggers = Object.keys(triggerToExplainerMap);
 		console.log('triggers list', triggers);
-		var pendingRebuttals = [];
+		var pendingExplainers = [];
 
-		_.each(triggerToRebuttalMap, function(explainerKey, trigger){
+		_.each(triggerToExplainerMap, function(explainerKey, trigger){
 			if (lowercasedStr.indexOf(trigger) >= 0){
 				var explainerItem = explainerDB[explainerKey];
-				pendingRebuttals.push(explainerItem);
+				pendingExplainers.push(explainerItem);
 			}
+		});
+
+		pendingExplainers = _.uniq(pendingExplainers);
+		
+		console.log('pendingExplainers', pendingExplainers);
+
+		_.each(pendingExplainers, function(explainerItem){
+			$list.append(createResultEl({
+				headline : explainerItem.headline,
+				explainer : explainerItem.explainer
+			}));
 		});
 	}
 
 	function createResultEl(data){
 		var str = '<li>' + 
 		'<h2 class="claim"></h2>' + 
-		'<p class="explainer"></p>' +
+		'<div class="explainer"></div>' +
 		'</li>';
 
 		var $el = $(str);
 
 		$el.find('.claim').text(data.headline);
-		$el.find('.explainer').text(data.explainer);
+		$el.find('.explainer').html('<p>' + data.explainer + '</p>');
 
 		return $el;
 	}
